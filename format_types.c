086e50a960721fb8025f98bb80e314228e8d1f49
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-23 16:56:10 +0200

    Add option to print octal numbers with %o

--- format_types.c
+++ format_types.c
@@ -6,6 +6,7 @@ struct format_specifier format_types[] = {
 	{'d', _print_integer},
 	{'i', _print_integer},
 	{'u', _print_unsigned},
+	{'o', _print_octal},
 	{'b', _print_binary},
 	{'%', NULL},
 	{'\0', NULL}
--- main.h
+++ main.h
@@ -13,6 +13,7 @@ int _print_char(va_list args);
 int _print_string(va_list args);
 int _print_integer(va_list args);
 int _print_unsigned(va_list args);
+int _print_octal(va_list args);
 int _print_binary(va_list args);
 
 /**
