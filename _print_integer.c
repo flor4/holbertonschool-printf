e7ec957f57d80839d19d0a97058b79974e1bc495
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-23 18:12:40 +0200

    Handle when int = INT_MIN

--- _print_integer.c
+++ _print_integer.c
@@ -10,8 +10,8 @@
 int _print_integer(va_list args)
 {
 	int num = va_arg(args, int);
-	int count = 0, div = 1;
-	unsigned int temp_num;
+	int count = 0;
+	unsigned int div = 1, abs_num;
 
 	if (num == 0) /* Handle zero explicitly */
 		return (_putchar('0'));
@@ -19,16 +19,14 @@ int _print_integer(va_list args)
 	if (num < 0) /* Handle negative numbers */
 	{
 		count += _putchar('-');
-		num = -num;
+		abs_num = (unsigned int)(-num); /* Convert to unsigned to handle INT_MIN */
 	}
 	else
-		num = num;
-
-	temp_num = num;
+		abs_num = (unsigned int)num;
 
 	/* Find the highest power of 10 */
 	/* (if number = 123 then we get 100) */
-	while (temp_num / div >= 10)
+	while (abs_num / div >= 10)
 		div *= 10;
 
 	/* Print each digit from left to right */
@@ -36,10 +34,10 @@ int _print_integer(va_list args)
 	{
 		/* Extract the current digit and convert it to a character */
 		/* If number = 123 and div = 100 then 123 / 100 = 1 */
-		count += _putchar((num / div) + '0');
+		count += _putchar((abs_num / div) + '0');
 		/* Remove the printed digit from the number */
 		/* If number = 123 and div = 100 then 123 % 100 = 23 */
-		num %= div;
+		abs_num %= div;
 		/* Move to the next lower digit */
 		/* If div = 100 then div / 10 = 10 */
 		div /= 10;
