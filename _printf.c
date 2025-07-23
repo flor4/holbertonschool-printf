a0acbb75547a641d2fd70c119eebedd29a576e77
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-23 15:41:56 +0200

    Add buffer to _putchar function for better perfs

--- _printf.c
+++ _printf.c
@@ -45,6 +45,7 @@ int _printf(const char *format, ...)
 			i++;
 		}
 	}
+	_putchar(-1);
 	va_end(args);
 	return (count);
 }
--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,41 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to print.
+ * _putchar - Writes a character to stdout using a static buffer.
+ * @c: The character to print, or -1 to flush the buffer.
  *
- * Return: On success, returns 1. On error, returns -1.
+ * Return: 1 on character write, 0 on flush, -1 on error.
  */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buffer[1024]; /* Buffer to store characters */
+	static int i;             /* Current index in buffer */
+	int written;              /* Number of bytes written or error */
+
+	/* If c == -1, flush the buffer */
+	if (c == -1)
+	{
+		if (i > 0) /* If buffer always has chars in */
+		{
+			written = write(1, buffer, i);
+			i = 0;
+			if (written < 0)
+				return (-1); /* Error during write */
+		}
+		return (0); /* Flushed */
+	}
+
+	buffer[i++] = c; /* Add character to buffer */
+
+	/* If buffer is full, flush it */
+	if (i == 1024)
+	{
+		written = write(1, buffer, i);
+		i = 0;
+		if (written < 0)
+			return (-1); /* Error during write */
+	}
+
+	return (1); /* Character buffered or written */
 }
