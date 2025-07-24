0301d7fbdb50724f545d3117ca633eb078ea46f1
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-24 21:37:48 +0200

    Changement for betty style

--- format_types.c
+++ format_types.c
@@ -1,18 +1,33 @@
 #include "main.h"
 
-struct format_specifier format_types[] = {
-	{'c', _print_char},
-	{'s', _print_string},
-	{'d', _print_integer},
-	{'i', _print_integer},
-	{'u', _print_unsigned},
-	{'o', _print_octal},
-	{'x', _print_lower_hexa},
-	{'X', _print_upper_hexa},
-	{'b', _print_binary},
-	{'S', _print_ascii},
-	{'p', _print_memory},
-	{'r', _print_reverse},
-	{'%', NULL},
-	{'\0', NULL}
-};
+/**
+ * get_format_types - Get array of format specifiers and handlers
+ *
+ * Description: Returns an array that contains pairs of format specifiers and
+ * their corresponding handler functions.
+ * Each entry maps a character (like 'c', 's', 'd')
+ * to a function that handles printing for that specific format.
+ *
+ * Return: Array of format_specifier structures
+ */
+format_specifier_t *get_format_types(void)
+{
+	static format_specifier_t format_types[] = {
+		{'c', _print_char},
+		{'s', _print_string},
+		{'d', _print_integer},
+		{'i', _print_integer},
+		{'u', _print_unsigned},
+		{'o', _print_octal},
+		{'x', _print_lower_hexa},
+		{'X', _print_upper_hexa},
+		{'b', _print_binary},
+		{'S', _print_ascii},
+		{'p', _print_memory},
+		{'r', _print_reverse},
+		{'%', NULL},
+		{'\0', NULL}
+	};
+
+	return (format_types);
+}
