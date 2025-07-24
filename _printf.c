4804d5bd575b9b33a3d50ac591f5a9abcec67fd7
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-24 20:57:14 +0200

    Update for betty style (few changes)

--- _printf.c
+++ _printf.c
@@ -10,25 +10,22 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, j = 0, count = 0;
+	int i, j = 0, count = 0;
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	if (format[0] == '\0')
 		return (0);
-
 	va_start(args, format);
-	while (format[i]) /* Loop through each character in the format string */
+	for (i = 0; format[i]; i++) /* Loop through each char in the format string */
 	{ /* Handle format specifiers */
 		if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == '\0')
+			if (format[++i] == '\0')
 				return (-1);
 			if (format[i] == '%')
 			{ /* Handle % at end of string */
 				count += _putchar('%');
-				i++;
 				continue;
 			}
 			for (j = 0; format_types[j].type != '\0'; j++)
@@ -44,16 +41,11 @@ int _printf(const char *format, ...)
 				count += _putchar('%');
 				count += _putchar(format[i]);
 			}
-			i++;
 		}
-		else
-		{ /* Print regular character */
+		else /* Print regular character */
 			count += _putchar(format[i]);
-			i++;
-		}
 	}
 	_putchar(-1);
 	va_end(args);
 	return (count);
 }
-
